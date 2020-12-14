20a0c22fb0fed7b27460aba31d82f56c576a7f13
jtreeves <jackson.t.reeves@gmail.com>
2020-12-14 04:22:40 -0500

    Delete Square's constructor because it no longer tracks state

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Square extends React.Component {
         return (
             <button
                 className="square"
-                onClick={() => alert('click')}
+                onClick={() => this.props.onClick({value: 'X'})}
             >
                 {this.props.value}
             </button>
@@ -16,8 +16,20 @@ class Square extends React.Component {
 }
   
 class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+    
     renderSquare(i) {
-        return <Square value={i} />
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        )
     }
   
     render() {
